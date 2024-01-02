6d885b223dcf6dc94438b6749dd8458f1cbb28e2
Lokie89 <traeuman@gmail.com>
2024-01-02 11:47:33 +0900

    Section3, Add,Mapping Servlet

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -4,11 +4,27 @@ import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactor
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 public class HellobootApplication {
 
     public static void main(String[] args) {
         ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-        WebServer webServer = serverFactory.getWebServer();
+        WebServer webServer = serverFactory.getWebServer(servletContext -> {
+            servletContext.addServlet("hello", new HttpServlet() {
+                @Override
+                protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+                    // 응답의 3요소 : STATUS, HEADERS ( Content Type ), BODY
+                    resp.setStatus(200);
+                    resp.setContentType("text/plain");
+                    resp.getWriter().println("Hello Servlet");
+                }
+            }).addMapping("/hello");
+        });
         webServer.start();
     }
 
