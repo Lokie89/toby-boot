25b16eac4cdee2ba5b4d35ec09a2b5d2586d52bc
Lokie89 <traeuman@gmail.com>
2024-01-02 12:53:08 +0900

    Section3, Front Controller

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -3,6 +3,9 @@ package tobyspring.helloboot;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -15,15 +18,26 @@ public class HellobootApplication {
     public static void main(String[] args) {
         ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
         WebServer webServer = serverFactory.getWebServer(servletContext -> {
-            servletContext.addServlet("hello", new HttpServlet() {
+            servletContext.addServlet("frontController", new HttpServlet() {
                 @Override
                 protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-                    // 응답의 3요소 : STATUS, HEADERS ( Content Type ), BODY
-                    resp.setStatus(200);
-                    resp.setContentType("text/plain");
-                    resp.getWriter().println("Hello Servlet");
+
+                    // front controller 역할을 하는 로직 작성
+                    if (req.getRequestURI().equals("/hello")
+                            && req.getMethod().equals(HttpMethod.GET.name())) {
+                        String name = req.getParameter("name");
+
+                        // 응답의 3요소 : STATUS, HEADERS ( Content Type ), BODY
+                        resp.setStatus(HttpStatus.OK.value());
+                        resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
+                        resp.getWriter().println("Hello " + name);
+                    } else if (req.getRequestURI().equals("/user")) {
+                        System.out.println("user 조회");
+                    } else {
+                        resp.setStatus(HttpStatus.NOT_FOUND.value());
+                    }
                 }
-            }).addMapping("/hello");
+            }).addMapping("/*");
         });
         webServer.start();
     }
